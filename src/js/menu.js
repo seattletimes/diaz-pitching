8594e2e83ff46977d835c42e7a60a308774c716b
Thomas Wilburn <news@thomaswilburn.net>
2017-03-29 10:22:37 -0700

    Move page titles into a sheet

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,3 +4,6 @@ var link = $.one(".main-nav .activate");
 var nav = $.one(".main-nav");
 
 link.addEventListener("click", () => nav.classList.toggle("open"));
+
+link.addEventListener("mouseenter", () => nav.classList.add("open"));
+nav.addEventListener("mouseleave", () => nav.classList.remove("open"));
